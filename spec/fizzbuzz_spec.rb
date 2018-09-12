ff9c1695eb4a618f4c7577a1a1d17662e37a282e
jebax <joel.eliot.baxter@gmail.com>
2018-09-12 19:04:34 +0100

    added extra test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,4 +1,5 @@
 require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'return "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
@@ -14,13 +15,24 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(12)).to eq "fizz"
   end
 
-  it 'return "fizzbuzz" when passed multiples of 15' do
+  it 'return "fizzbuzz" when passsed 15' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'return "fizzbuzz" when passed other multiples of 15' do
     expect(fizzbuzz(30)).to eq "fizzbuzz"
+    expect(fizzbuzz(45)).to eq "fizzbuzz"
   end
 
   it 'return number when passed 7' do
     expect(fizzbuzz(7)).to eq 7
+  end
+
+  it 'return number when passed various other numbers' do
+    expect(fizzbuzz(19)).to eq 19
+    expect(fizzbuzz(58)).to eq 58
+    expect(fizzbuzz(151)).to eq 151
+    expect(fizzbuzz(12451097)).to eq 12451097
   end
 
   it 'return "buzz" when passed other multiples of 5' do
