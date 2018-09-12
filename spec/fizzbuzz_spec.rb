19f2e67b0bbd5e8ace4b5a03bba14c9214b994bc
jebax <joel.eliot.baxter@gmail.com>
2018-09-12 19:05:34 +0100

    reordered tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,14 +9,20 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq "buzz"
   end
 
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
   it 'return "fizz" when passed other multiples of 3' do
     expect(fizzbuzz(6)).to eq "fizz"
     expect(fizzbuzz(9)).to eq "fizz"
     expect(fizzbuzz(12)).to eq "fizz"
   end
 
-  it 'return "fizzbuzz" when passsed 15' do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  it 'return "buzz" when passed other multiples of 5' do
+    expect(fizzbuzz(10)).to eq "buzz"
+    expect(fizzbuzz(25)).to eq "buzz"
+    expect(fizzbuzz(20)).to eq "buzz"
   end
 
   it 'return "fizzbuzz" when passed other multiples of 15' do
@@ -33,12 +39,6 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(58)).to eq 58
     expect(fizzbuzz(151)).to eq 151
     expect(fizzbuzz(12451097)).to eq 12451097
-  end
-
-  it 'return "buzz" when passed other multiples of 5' do
-    expect(fizzbuzz(10)).to eq "buzz"
-    expect(fizzbuzz(25)).to eq "buzz"
-    expect(fizzbuzz(20)).to eq "buzz"
   end
 
 end
