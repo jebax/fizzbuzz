c313508529b9eb8a313613c829d47ef7b2e7b40b
jebax <joel.eliot.baxter@gmail.com>
2018-09-12 18:14:31 +0100

    completed exercise

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,30 @@ describe 'fizzbuzz' do
   it 'return "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'return "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it 'return "fizz" when passed other multiples of 3' do
+    expect(fizzbuzz(6)).to eq "fizz"
+    expect(fizzbuzz(9)).to eq "fizz"
+    expect(fizzbuzz(12)).to eq "fizz"
+  end
+
+  it 'return "fizzbuzz" when passed multiples of 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end
+
+  it 'return number when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+
+  it 'return "buzz" when passed other multiples of 5' do
+    expect(fizzbuzz(10)).to eq "buzz"
+    expect(fizzbuzz(25)).to eq "buzz"
+    expect(fizzbuzz(20)).to eq "buzz"
+  end
+
 end
